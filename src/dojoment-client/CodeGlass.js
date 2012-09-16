a1cb2affdcaa5532111c8b49e231f4584e401cf7
Kitson Kelly <dojo@kitsonkelly.com>
2012-09-16 19:47:47 +0100

    i18n labels for CodeGlass and update refdocs

--- src/dojoment-client/CodeGlass.js
+++ src/dojoment-client/CodeGlass.js
@@ -26,10 +26,11 @@ define([
 	"dojo/text!./resources/CodeGlassCodeTemplate.html",
 	"dojo/text!./resources/CodeGlassExportTemplate.html",
 	"dojo/i18n!dijit/nls/loading",
+	"dojo/i18n!./nls/CodeGlass",
 	"dojo/sniff"
 ], function(array, config, declare, baseFx, lang, aspect, domClass, domConst, domGeom, style, JSON, has, on, query,
 		_OnDijitClickMixin, _TemplatedMixin, _WidgetBase, Dialog, Toolbar, Button, BorderContainer, ContentPane,
-		TabContainer, template, codeTemplate, exportTemplate, i18nLoading){
+		TabContainer, template, codeTemplate, exportTemplate, i18nLoading, labels){
 
 	var scriptOpen = "<scr" + "ipt>",
 		scriptClose = "</" + "scri" + "pt>",
@@ -111,21 +112,7 @@ define([
 		templateString: template,
 
 		// labels: Object
-		labels: {
-			run: "<i class='icon-play'></i> Run",
-			runTitle: "Run Code Example",
-			code: "<i class='icon-file'></i> Code",
-			codeTitle: "Display Source Code",
-			collapse: "<i class='icon-chevron-up'></i> Collapse",
-			collapseTitle: "Collapse Code Example",
-			expand: "<i class='icon-chevron-down'></i> Expand",
-			expandTitle: "Expand Code Example",
-			example: "<i class='icon-play'></i> Example",
-			copy: "<i class='icon-copy'></i> Select All",
-			copyTitle: "Select all the Code",
-			"export": "<i class='icon-share'></i> Export",
-			exportTitle: "Export to JSFiddle"
-		},
+		labels: labels,
 
 		buildRendering: function(){
 			var nlTextArea = query("textarea.parts", this.srcNodeRef),
---
+++ src/dojoment-client/nls/CodeGlass.js
@@ -0,0 +1,17 @@
+define({
+	root:({
+		run: "<i class='icon-play'></i> Run",
+		runTitle: "Run Code Example",
+		code: "<i class='icon-file'></i> Code",
+		codeTitle: "Display Source Code",
+		collapse: "<i class='icon-chevron-up'></i> Collapse",
+		collapseTitle: "Collapse Code Example",
+		expand: "<i class='icon-chevron-down'></i> Expand",
+		expandTitle: "Expand Code Example",
+		example: "<i class='icon-play'></i> Example",
+		copy: "<i class='icon-copy'></i> Select All",
+		copyTitle: "Select all the Code",
+		"export": "<i class='icon-share'></i> Export",
+		exportTitle: "Export to JSFiddle"
+	})
+});
